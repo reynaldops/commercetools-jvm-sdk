1b6bcc5c97ecb72a1ffbfe434967c4a7fcd4a11a
mkoester <matthias.koester@commercetools.de>
2017-03-07 13:26:40 +0100

    Replaced product discount impl classes with generated classes.

--- commercetools-models/src/main/java/io/sphere/sdk/productdiscounts/DiscountedPrice.java
+++ commercetools-models/src/main/java/io/sphere/sdk/productdiscounts/DiscountedPrice.java
@@ -1,10 +1,12 @@
 package io.sphere.sdk.productdiscounts;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.sphere.sdk.annotations.ResourceValue;
 import io.sphere.sdk.models.Reference;
 
 import javax.money.MonetaryAmount;
 
+@ResourceValue
 @JsonDeserialize(as = DiscountedPriceImpl.class)
 public interface DiscountedPrice {
     MonetaryAmount getValue();
@@ -12,6 +14,6 @@ public interface DiscountedPrice {
     Reference<ProductDiscount> getDiscount();
 
     static DiscountedPrice of(final MonetaryAmount value, final Reference<ProductDiscount> discount) {
-        return new DiscountedPriceImpl(value, discount);
+        return new DiscountedPriceImpl(discount, value);
     }
 }
